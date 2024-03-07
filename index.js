1b7de7ee63ca281a01ce888d26bc5f040daaf6cd
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-03-07 13:58:45 +0600

    Get Food Items Data

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const port = process.env.PORT || 3000;
 require('dotenv').config()
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
 const uri = `mongodb+srv://${process.env.RESTURANT_SECRET_NAME}:${process.env.RESTURANT_SECRET_PASSWORD}@cluster0.2xcsswz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -20,8 +21,20 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+    // COLLECTIONS DECLARATIONS
+
+    const foodItemCollections = client.db("restruantDB").collection("foodCollections")
+
+    // GET FOOD ITEMS
+    app.get('/resturant/api/v1/fooditems', async(req,res) => {
+      const cursor = await foodItemCollections.find().toArray();
+      res.send(cursor)
+    })
+
+
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
